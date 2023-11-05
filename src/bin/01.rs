b18ed52a744bb3da9dd7cbba941a69cc5eae677b
mrbarge <matt.bargenquast@gmail.com>
2023-11-05 21:05:47 +1000

    Day 01 added

---
+++ src/bin/01.rs
@@ -0,0 +1,51 @@
+pub fn part_one(input: &str) -> Option<i32> {
+
+    let mut floor = 0;
+
+    for c in input.chars() {
+        if c == '(' {
+            floor += 1;
+        } else if c == ')' {
+            floor -= 1;
+        }
+    }
+
+    Some(floor)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let mut position: u32 = 1;
+    let mut floor = 0;
+
+    for c in input.chars() {
+        if c == '(' {
+            floor += 1;
+        } else if c == ')' {
+            floor -= 1;
+        }
+        if floor == -1 {
+            break;
+        }
+        position += 1;
+    }
+    Some(position)
+}
+
+advent_of_code::main!(1);
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", 1));
+        assert_eq!(result, None);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", 1));
+        assert_eq!(result, None);
+    }
+}
